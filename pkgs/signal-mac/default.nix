aad78c507415ace5f39775618895662659abc538
Tony Höglund <tony.hoglund@gmail.com>
2022-03-07 10:23:22 +0100

    signal-mac: 5.24.0 -> 5.34.0

--- pkgs/signal-mac/default.nix
+++ pkgs/signal-mac/default.nix
@@ -1,13 +1,20 @@
 { lib, stdenv, fetchurl }:
 let
   pname = "signal";
-  inherit (import ./settings.nix) version;
+  inherit (import ./settings.nix) version sha512;
+
+  hostSystem = stdenv.hostPlatform.system;
+  platform = {
+    x86_64-darwin = "x64";
+    aarch64-darwin = "arm64";
+  }.${hostSystem} or (throw "Unsupported system: ${hostSystem}");
+
 in stdenv.mkDmgDerivation {
   inherit pname version;
 
   src = fetchurl {
-    url = "https://updates.signal.org/desktop/signal-desktop-mac-${version}.dmg";
-    sha256 = "5c498af19e30bb37d677a614f2c36c4e6284f89cf017413086828c082a062823";
+    url = "https://updates.signal.org/desktop/signal-desktop-mac-${platform}-${version}.dmg";
+    sha512 = sha512.${platform} or (throw "Missing hash for host system: ${hostSystem}");
   };
 
   meta = {
