779f3839dd5b0efef2bc07c05c4efa0e8a27633b
Tony Höglund <tony.hoglund@gmail.com>
2022-04-07 12:42:19 +0200

    spotify-mac: add update script 1.1.71.560.gc21c3367-40 -> 1.1.81.604.gccacfc8c-16

--- pkgs/spotify-mac/default.nix
+++ pkgs/spotify-mac/default.nix
@@ -1,17 +1,27 @@
 { lib, stdenv, fetchurl }:
 let
   pname = "spotify";
-  version = "1.1.71.560.gc21c3367-40";
+  version = "1.1.81.604.gccacfc8c-16";
+
+  src = {
+    x86_64-darwin = {
+      url = "https://download.scdn.co/Spotify.dmg";
+      sha512 = "1dh0lc0dn12n5km119vjvfmfyn7c6w102xgjify138vbhhqxk2kcygpp8qnddb53gpp7mmhlw963x5wzksmmfbc1dr3i5cqblpgxad5";
+    };
+    aarch64-darwin = {
+      url = "https://download.scdn.co/SpotifyARM64.dmg";
+      sha512 = "1h0rz0gsqywdawpfkaifw8f7xygqh18kigfjczjxwqn2hb6gz80pxk9p0xz6wwm0mkzy6zlnk0byxlmbc3f24cq1mqgkr1v5z71zyss";
+    };
+  }.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
 in stdenv.mkDmgDerivation {
   inherit pname version;
 
-  src = fetchurl {
-    url = "https://download.scdn.co/Spotify.dmg";
-    sha256 = "1m8a2rj1pwxcmszsnnx5m2sg3ipwdfg4zd4879r2r35s2bwsdv91";
-  };
+  src = fetchurl src;
+
+  passthru.updateScript = ./update.sh;
 
   meta = {
-    homepage = "https://spotify.com";
+    homepage = "https://www.spotify.com";
     license = lib.licenses.unfree;
   };
 }
---
+++ pkgs/spotify-mac/update.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i bash -p curl mktemp undmg xmlstarlet common-updater-scripts
+
+set -eou pipefail
+
+url () {
+  local platform=${1:-}
+  echo "https://download.scdn.co/Spotify${platform}.dmg"
+}
+
+temp_dir=$(mktemp -d)
+pushd "$temp_dir"
+trap "rm -fr $temp_dir" EXIT
+
+curl $(url) --output Spotify.dmg
+undmg Spotify.dmg
+version=$(xmlstarlet sel --net -t -m "/plist/dict/key[.='CFBundleVersion']" -v "following-sibling::string[1]" ./Spotify.app/Contents/Info.plist)
+
+popd
+echo Using version "$version"
+
+platforms=( \
+  "x86_64-darwin   " \
+  "aarch64-darwin  ARM64" \
+)
+for kv in "${platforms[@]}"; do
+  nix_platform=${kv%% *}
+  platform=${kv##* }
+  file_url=$(url "$platform")
+
+  sha512=$(nix-prefetch-url --type sha512 "$file_url")
+
+  update-source-version spotify-mac 0 "0000000000000000000000000000000000000000000000000000" --system="$nix_platform" --file=./pkgs/spotify-mac/default.nix
+  update-source-version spotify-mac "$version" "$sha512" --system="$nix_platform" --file=./pkgs/spotify-mac/default.nix
+done
