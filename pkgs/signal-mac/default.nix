afe1d60a1106e91325c8102b1574340efb09de5e
Tony Höglund <tony.hoglund@gmail.com>
2022-04-07 10:20:56 +0200

    signal-mac: add update script 5.34.0 -> 5.37.0

--- pkgs/signal-mac/default.nix
+++ pkgs/signal-mac/default.nix
@@ -1,7 +1,12 @@
 { lib, stdenv, fetchurl }:
 let
   pname = "signal";
-  inherit (import ./settings.nix) version sha512;
+  version = "5.37.0";
+
+  sha512 = {
+    x64 = "0y1kwf9i5bhhz50j38d9f2v25503cjcad6as753j7fb127vk0ngzl49rmlgyb9fkvyn7y7vqq8clzbadnf4yk7r0d11d0660sd6gj7a";
+    arm64 = "2garysp1myn9f5b4xz7jc20jxnxad5x0g08pxk3g0mibq9pi9kkhnm3njxj4hbz0vqds86k60sgrs7hf6pkybsfavz9fm1izc18cgab";
+  };
 
   hostSystem = stdenv.hostPlatform.system;
   platform = {
@@ -16,6 +21,8 @@ in stdenv.mkDmgDerivation {
     url = "https://updates.signal.org/desktop/signal-desktop-mac-${platform}-${version}.dmg";
     sha512 = sha512.${platform} or (throw "Missing hash for host system: ${hostSystem}");
   };
+
+  passthru.updateScript = ./update.sh;
 
   meta = {
     homepage = "https://signal.com";
