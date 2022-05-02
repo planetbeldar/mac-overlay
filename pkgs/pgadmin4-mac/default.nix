8bb07db2c9bcda0d5c6fa4214b4490d5efc9203d
Tony Höglund <tony.hoglund@gmail.com>
2022-05-02 14:12:18 +0200

    pgadmin4-mac: 6.7 -> 6.8

--- pkgs/pgadmin4-mac/default.nix
+++ pkgs/pgadmin4-mac/default.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, fetchurl }:
 let
   pname = "pgadmin4";
-  version = "6.7";
+  version = "6.8";
 in stdenv.mkDmgDerivation {
   inherit pname version;
 
   src = fetchurl {
     url = "https://ftp.postgresql.org/pub/pgadmin/pgadmin4/v${version}/macos/pgadmin4-${version}.dmg";
-    sha256 = "39ccc32418c59334fc893d863427697f220f3f9ea8da04450cd65d90107db085";
+    sha256 = "DH/2t+0rhow7WRySxMwxJxSf/nbYX8hUZMG77KpqqeQ=";
   };
 
   passthru.updateScript = ./update.sh;
