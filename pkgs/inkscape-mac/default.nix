c5e157f79edf0a6efe081fce8abd33134b0ad073
Tony Höglund <tony.hoglund@gmail.com>
2022-05-21 19:38:41 +0200

    inkscape: 1.1.2 -> 1.2.0

--- pkgs/inkscape-mac/default.nix
+++ pkgs/inkscape-mac/default.nix
@@ -1,14 +1,23 @@
 { lib, stdenv, fetchurl }:
 let
   pname = "inkscape";
-  version = "1.1.2";
+  version = "1.2.0";
+
+  url = arch: "https://media.inkscape.org/dl/resources/file/Inkscape-${version}_${arch}.dmg";
+  src = {
+    x86_64-darwin = {
+      url = url "x86_64";
+      sha512 = "3ajj0620c1m0060gkk43is10cq3yq861ps3hcxn7iqfddnvdndyszj4m3lvkmp0bcga1icv2s5zm6988xky95jfqqjl1s922avxsin7";
+    };
+    aarch64-darwin = {
+      url = url "arm64";
+      sha512 = "12amc116pgs9gw0anbbpf4bv11kfai92s2j4sgcm51ibfnl0xl71l8n3j4x01sl2ji9shz0hw66x4pkm37bl2iyf74rds78ybxd5nqb";
+    };
+  }.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
 in stdenv.mkDmgDerivation {
   inherit pname version;
 
-  src = fetchurl {
-    url = "https://media.inkscape.org/dl/resources/file/Inkscape-${version}.dmg";
-    sha256 = "WvL8+mXGjHaIuGK6nFCRmb4hlU8ozPu31ABM+T008Yo=";
-  };
+  src = fetchurl src;
 
   passthru.updateScript = ./update.sh;
 
