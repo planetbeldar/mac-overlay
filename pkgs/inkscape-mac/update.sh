1b0a4b1adca7583d121c5d901708364cfeb072eb
Tony Höglund <tony.hoglund@gmail.com>
2022-05-21 19:37:10 +0200

    fix(inkscape): update script multiple platforms
    Adopt the update script to use different platform sources

--- pkgs/inkscape-mac/update.sh
+++ pkgs/inkscape-mac/update.sh
@@ -3,8 +3,16 @@
 
 set -eou pipefail
 
+url () {
+  local version=${1}
+  local platform=${2}
+
+  echo "https://media.inkscape.org/dl/resources/file/Inkscape-${version}_${platform}.dmg"
+}
+
 name="inkscape-mac"
 version="$(curl -sIL 'https://inkscape.org/release' | grep -oP '(?<=location: /release/inkscape-)\d+\.\d+(.\d+)?(?=\/)')"
+version=$([[ "$version" =~ ^[0-9]+\.[0-9]+$ ]] && echo "$version.0" || echo "$version") # add patch part to version
 currentVersion=$(nix-instantiate --eval -E "with import ./.; $name.version" | tr -d '"')
 
 if [[ "$version" == "$currentVersion" ]]; then
@@ -14,4 +22,17 @@ fi
 
 echo "Using $version"
 
-update-source-version "$name" "$version" --file=./pkgs/"$name"/default.nix
+platforms=( \
+  "x86_64-darwin  x86_64" \
+  "aarch64-darwin arm64" \
+)
+for kv in "${platforms[@]}"; do
+  nix_platform=${kv%% *}
+  platform=${kv##* }
+  file_url=$(url "$version" "$platform")
+
+  sha512=$(nix-prefetch-url --type sha512 "$file_url")
+
+  update-source-version "$name" 0 "0000000000000000000000000000000000000000000000000000" --system="$nix_platform" --file=./pkgs/"$name"/default.nix
+  update-source-version "$name" "$version" "$sha512" --system="$nix_platform" --file=./pkgs/"$name"/default.nix
+done
