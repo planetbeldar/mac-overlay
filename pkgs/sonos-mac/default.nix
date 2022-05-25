1900036c600f22bbc42991d9c2e79c49d60bb6fd
Tony Höglund <tony.hoglund@gmail.com>
2022-05-25 09:50:29 +0200

    sonos-mac: 14.6 -> 14.8

--- pkgs/sonos-mac/default.nix
+++ pkgs/sonos-mac/default.nix
@@ -1,7 +1,7 @@
 { lib, stdenv, fetchurl }:
 let
   pname = "sonos";
-  version = "14.6";
+  version = "14.8";
 
   fileVersion = builtins.replaceStrings [ "." ] [ "" ] version;
 in stdenv.mkDmgDerivation {
@@ -9,7 +9,7 @@ in stdenv.mkDmgDerivation {
 
   src = fetchurl {
     url = "https://update.sonos.com/software/mac/mdcr/SonosDesktopController${fileVersion}.dmg";
-    sha512 = "5TS1FWfTOiZmyj/g1Hu1v+Iceg9UPuocwnFM35B68E+3maRfRaL/2LFPpJCYGgurMMVOe/PxqljrSILp9dvoAw==";
+    sha512 = "Aexz1sLw1+Rv7qsY7YAdJvUY7j0k6PfZVwv5+ZnKVSh6DgM+wv4BE8tgqQSSqsiBEsFg/+suAllqEdshCUqTnw==";
   };
 
   passthru.updateScript = ./update.sh;
