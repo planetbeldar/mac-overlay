c0e5f5ee28199a63b17e3df90075f732caa38d99
Tony Höglund <tony.hoglund@gmail.com>
2022-08-30 09:10:03 +0200

    sonos-mac: 14.12,69.1.31120 -> 14.14,69.1.32100

--- pkgs/sonos-mac/default.nix
+++ pkgs/sonos-mac/default.nix
@@ -5,19 +5,21 @@ let
   inherit (builtins) concatStringsSep;
 
   pname = "sonos";
-  version = "14.12,69.1.31120";
+  version = "14.14,69.1.32100";
 
-  versions = splitString "," version;
-  fileVersion = let
-    versionNumbers = splitString "." (last versions);
-  in "${concatStringsSep "." (take 2 versionNumbers)}-${last versionNumbers}";
+  # versions = splitString "," version;
+  # fileVersion = let
+  #   versionNumbers = splitString "." (last versions);
+  # in "${concatStringsSep "." (take 2 versionNumbers)}-${last versionNumbers}";
 
 in stdenv.mkDmgDerivation {
   inherit pname version;
 
   src = fetchurl {
-    url = "https://update-software.sonos.com/software/flfowmpo/Sonos_${fileVersion}.dmg";
-    sha512 = "rZrfeuy3oXIJrddMuRDQhRynCTg/dEvtJx6h63iyqbg357L7X0Dc9JvnydwQbv3pHG2KIc9fOxJqP/H/xZOXkQ==";
+    # url = "https://update-software.sonos.com/software/flfowmpo/Sonos_${fileVersion}.dmg";
+    # url changes with every release, use redirect url for now
+    url = "https://www.sonos.com/en/redir/controller_software_mac2";
+    sha512 = "eVHzehKIO0aGcQ6WPbNQeVxkim8w3w+vGZKJGBKXz0mfH1JzwciMadD4cu/GiMKqCz7lpM8j3VlboLTMW81Scg==";
   };
 
   passthru.updateScript = ./update.sh;
