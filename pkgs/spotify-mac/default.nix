8ebaacf2fd80a173e777124e1841d09e6deec063
Tony Höglund <tony.hoglund@gmail.com>
2022-08-30 09:08:26 +0200

    spotify-mac: 1.1.91.824 -> 1.1.92.647

--- pkgs/spotify-mac/default.nix
+++ pkgs/spotify-mac/default.nix
@@ -1,16 +1,16 @@
 { lib, stdenv, fetchurl }:
 let
   pname = "spotify";
-  version = "1.1.91.824.g07f1e963-9";
+  version = "1.1.92.647.ga4397eb7-7";
 
   src = {
     x86_64-darwin = {
       url = "https://download.scdn.co/Spotify.dmg";
-      sha512 = "3w8figfq578fainrahi3xqxkps6lkpv3kj8i7x1s9a7fgsnv1qpi6k2cvzssq1nkawjp90vfvp4mwzhm09ddaphz3vpc9hz61k6z2a8";
+      sha512 = "07j2c81n65nnxg4jpp80r5lx5m9bxrsl2gskg0h0csvk5s8ni83qap9ibjal9gyvpy8xlzfbj25f56jxgc0gc0kbmqrlw3xkigqps78";
     };
     aarch64-darwin = {
       url = "https://download.scdn.co/SpotifyARM64.dmg";
-      sha512 = "08704avbnf7pi1fd79xyw1glgi6ndy44gybaqyiy0h0lrl1djcbj5982ssyspd5fg66aqzkzqzvip0vr35hx9njd5kiar0pr5g02gil";
+      sha512 = "1bib87y801gg93llkbcks40mnsvganrj5fpgd4ikiiwj3mxx4vdy5ay9mxdql4ab7bwnmw1p8qivpjb7g7i9dr4m4jbd7a7cj9aa1df";
     };
   }.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
 in stdenv.mkDmgDerivation {
