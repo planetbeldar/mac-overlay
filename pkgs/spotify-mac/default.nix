1a098f8f97b37ec9be22ead1306c1593655a3205
Tony Höglund <tony.hoglund@gmail.com>
2023-01-26 18:55:17 +0100

    spotify-mac: 1.2.0.1165 -> 1.2.3.1115

--- pkgs/spotify-mac/default.nix
+++ pkgs/spotify-mac/default.nix
@@ -1,16 +1,16 @@
 { lib, stdenv, fetchurl }:
 let
   pname = "spotify";
-  version = "1.2.0.1165.gabf054ab-1083";
+  version = "1.2.3.1115.gd61a8f5c-384";
 
   src = {
     x86_64-darwin = {
       url = "https://download.scdn.co/Spotify.dmg";
-      sha512 = "sha512-AV8StFW99pFT97I6hGKfD6eIx6Dotr1khO8+Ys1TVl/ry02lkCgx0raX2rcX/qC/iGXK2JcmL/NMeZNbhEcweQ==";
+      sha512 = "sha512-RFPS+l0nON8aZa//K+KOiED91sif4CZnVkJ5Ao2xRX2vru7phBq6eX6Q+vaAOXm9ocLCxZ+ktTOWOomtPX/IXg==";
     };
     aarch64-darwin = {
       url = "https://download.scdn.co/SpotifyARM64.dmg";
-      sha512 = "sha512-H7sWpfXdQiubf6d3tb3gb+kDbxig7PM9kBlcKT7mdWbdQDOR7juUjYwIWLXxozGeoVp7kVe9ghRuiY+MPTGuyQ==";
+      sha512 = "sha512-E8REcCBQfWVKDyq/wHGQ64ivqAWoH7AuXOy9U6BEPXs/o9sAaECQ3zbmXycsCOyLIRaean1MfUK/dgj4Xs6vOA==";
     };
   }.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
 in stdenv.mkDmgDerivation {
