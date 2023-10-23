14d9d43d618b245c6e9eaa379151272b81c71212
Tony Höglund <tony.hoglund@gmail.com>
2023-10-23 12:54:28 +0200

    sonos-mac: 15.8 -> 15.9

--- pkgs/sonos-mac/default.nix
+++ pkgs/sonos-mac/default.nix
@@ -1,15 +1,15 @@
 { lib, stdenv, fetchurl }:
 let
   pname = "sonos";
-  version = "15.8";
+  version = "15.9";
 
 in stdenv.mkDmgDerivation {
   inherit pname version;
 
   src = fetchurl {
     # Random url segment and filename with internal/build version numbers
-    url = "https://update-software.sonos.com/software/fnwewsgg/Sonos_75.1-45120.dmg";
-    sha512 = "sha512-tkwmSMgOZdQ4r2rfE4d7K8aYlPTS8Rd2JWbcNa+TSBoXU9dPxCgRg5KuSn3+B7hpa6nwB0WvSZAWIcZZKRw31Q==";
+    url = "https://update-software.sonos.com/software/irbxsaud/Sonos_75.1-46030.dmg";
+    sha512 = "sha512-oCpOQW35LLVibeeL+Fo+ZiKyMj4bde17Y2yUx3bkrTR7NqdRmxZoptd8avcEC6BOKlBITfTnnHZRhkXO5tbdNg==";
   };
 
   passthru.updateScript = ./update.sh;
