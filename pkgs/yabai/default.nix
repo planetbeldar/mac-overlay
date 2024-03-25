93b7059c25653209313792f1ffa87dd8f4444865
Tony Höglund <tony.hoglund@gmail.com>
2024-03-25 09:21:23 +0100

    yabai: 6.0.0 -> 7.0.3

--- pkgs/yabai/default.nix
+++ pkgs/yabai/default.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, fetchzip }:
 let
   pname = "yabai";
-  version = "6.0.0";
+  version = "7.0.3";
 in stdenv.mkDerivation {
   inherit pname version;
 
   src = fetchzip {
     url = "https://github.com/koekeishiya/yabai/releases/download/v${version}/yabai-v${version}.tar.gz";
-    sha256 = "KeZ5srx9dfQN9u6Fgg9BtIhLhFWp975iz72m78bWINo=";
+    sha256 = "EvtKYYjEmLkJTnc9q6f37hMD1T3DBO+I1LfBvPjCgfc=";
   };
 
   installPhase = ''
