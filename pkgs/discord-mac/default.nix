0aa529db7b8e89643cab41f4ff8ec220602daebf
Tony Höglund <tony.hoglund@gmail.com>
2024-06-13 12:55:31 +0200

    discord-mac: 0.0.297 -> 0.0.307

--- pkgs/discord-mac/default.nix
+++ pkgs/discord-mac/default.nix
@@ -1,13 +1,13 @@
 { lib, stdenv, fetchurl }:
 let
   pname = "discord";
-  version = "0.0.297";
+  version = "0.0.307";
 in stdenv.mkDmgDerivation {
   inherit pname version;
 
   src = fetchurl {
     url = "https://dl.discordapp.net/apps/osx/${version}/Discord.dmg";
-    sha256 = "ARXKLpfB3xpa8C/17p7ONJAySnt8/A+wgOMe9J9hFj8=";
+    sha256 = "FBYxQhtwctMQ8ByOgAVncWh5297k1Vh95w/rWnZg9Fw=";
   };
 
   passthru.updateScript = ./update.sh;
