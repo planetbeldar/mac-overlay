7958e8a3956559d6060890a1df2ae209e2a7b405
Tony Höglund <tony.hoglund@gmail.com>
2026-01-07 19:52:17 +0100

    signal-mac: 7.12.0 -> 7.83.0

--- pkgs/signal-mac/default.nix
+++ pkgs/signal-mac/default.nix
@@ -1,25 +1,19 @@
-{ lib, stdenv, fetchurl }:
+{
+  lib,
+  stdenv,
+  fetchurl,
+}:
 let
   pname = "signal";
-  version = "7.12.0";
+  version = "7.83.0";
 
-  sha512 = {
-    x64 = "2nf5qhbzwlnnknapvs1fs33dqymak8vf0lnwzpdgy91pz545cs918ijh6r07sjhy5akjbiynn9py7ss2xlhpqqd1sb4q1sgrhf45hsf";
-    arm64 = "0s33gg3blr98bidf7db6gmzpvrvp7y3mqfimc5f05bn8qhfy1cik1114hcngg6d726b42cilq6bimx09zqj739vch6ns2aca2xmn5sg";
-  };
-
-  hostSystem = stdenv.hostPlatform.system;
-  platform = {
-    x86_64-darwin = "x64";
-    aarch64-darwin = "arm64";
-  }.${hostSystem} or (throw "Unsupported system: ${hostSystem}");
-
-in stdenv.mkDmgDerivation {
+in
+stdenv.mkDmgDerivation {
   inherit pname version;
 
   src = fetchurl {
-    url = "https://updates.signal.org/desktop/signal-desktop-mac-${platform}-${version}.dmg";
-    sha512 = sha512.${platform} or (throw "Missing hash for host system: ${hostSystem}");
+    url = "https://updates.signal.org/desktop/signal-desktop-mac-universal-${version}.dmg";
+    sha512 = "2E2pTfQpSIS4E+swNyVhgH09AsQ0kTOSs9ttcLuf6F9fra6TD5QBdxJBPxcWVp2gVFljN6ymSOiMezoxyZRNeA==";
   };
 
   passthru.updateScript = ./update.sh;
@@ -29,6 +23,9 @@ in stdenv.mkDmgDerivation {
     changelog = "https://github.com/signalapp/Signal-Desktop/releases/tag/v${version}";
     description = "Private, simple, and secure messenger";
     license = lib.licenses.agpl3Only;
-    platforms = [ "x86_64-darwin" "aarch64-darwin" ];
+    platforms = [
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
   };
 }
--- pkgs/signal-mac/update.sh
+++ pkgs/signal-mac/update.sh
@@ -15,15 +15,4 @@ fi
 
 echo "Using version $version"
 
-platforms=( \
-  "x86_64-darwin  x64" \
-  "aarch64-darwin arm64" \
-)
-for kv in "${platforms[@]}"; do
-  nix_platform=${kv%% *}
-  platform=${kv##* }
-
-  sha512=$(nix-prefetch-url --type sha512 "https://updates.signal.org/desktop/signal-desktop-mac-$platform-$version.dmg")
-  update-source-version "$name" 0 "0000000000000000000000000000000000000000000000000000" --system="$nix_platform" --file=./pkgs/"$name"/default.nix
-  update-source-version "$name" "$version" "$sha512" --system="$nix_platform" --file=./pkgs/"$name"/default.nix
-done
+update-source-version "$name" "$version" --file=./pkgs/"$name"/default.nix
